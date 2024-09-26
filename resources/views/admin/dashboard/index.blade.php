8d2ead9591b4d14ea4450f54bd97f9c3b6db9cf6
longld <longld.8x@gmail.com>
2024-09-26 23:38:25 +0700

    main

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,17 +1,16 @@
 @extends('admin.layouts.app')
 @push('style')
-@endpush 
+@endpush
 @section('content')
-<section class="app-content "> 
-    <div class="card card-secondary card-outline mb-4 mt-4 search-box">
-        <h1 class="text-center m-6">Dashboard</h1>
-    </br>
-    </br>
-    </br>
-    </br>
-    </div>
-</section>
-
+    <section class="app-content ">
+        <div class="card card-secondary card-outline mb-4 mt-4 search-box">
+            <h1 class="text-center m-6">Dashboard</h1>
+            </br>
+            </br>
+            </br>
+            </br>
+        </div>
+    </section>
 @endsection
 @push('scripts')
 @endpush
